806b5bd618b6041ffda15523ee8c32aa7699099e
taylor.martin <taylor.martin@kakaoenterprise.com>
2023-02-28 21:24:55 +0900

    -

---
+++ wav_for_mos/make_csv_cmos.py
@@ -0,0 +1,24 @@
+import glob
+import os
+import random
+
+prefix = 'https://kallavinka8045.github.io/is2023/wav_for_mos/'
+domain_list = ['analysis-synthesis', 'multispk', 'fewshot', 'zeroshot']
+category_source = 'proposed'
+
+for domain in domain_list:
+    category_list = os.listdir(domain)
+    category_list.remove(category_source)
+    wav_name_list = os.listdir(os.path.join(domain, category_list[0]))
+    with open('audio_url_cmos_{}.csv'.format(domain), 'w', encoding='utf-8') as f:
+        for i in range(2):
+            if i != 0:
+                f.write(',')
+            f.write('audio_url{}'.format(i))
+        f.write('\n')
+        for wav_name in wav_name_list:
+            for i, category in enumerate(category_list):
+                f.write(os.path.join(prefix, domain, category_source, wav_name))
+                f.write(',')
+                f.write(os.path.join(prefix, domain, category, wav_name))
+                f.write('\n')
